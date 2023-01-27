9ed490fc2e2816e7dad6b5e88bf7649d90802581
MatBrands_B550_Arch <matheusmibrandao@gmail.com>
2023-01-26 23:39:49 -0300

    Update: Names

--- registration_system/model/Registration.py
+++ registration_system/model/Registration.py
@@ -24,6 +24,9 @@ class Registration:
         if user_data['id'] in data:
             return False
         
+        if (not user_data['name']) or (not user_data['telephone']) or (not user_data['address']):
+            return False
+        
         new_user = {
             user_data['id']: {
                 'name': user_data['name'],
@@ -37,45 +40,6 @@ class Registration:
         with open(path, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=4, ensure_ascii=False)
         return True
-    
-    def update_user(self, **user_data: dict) -> bool:
-        path = self.path
-        with open(path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-
-        if user_data['id'] not in data:
-            return False
-
-        if not data[user_data['id']]['status']:
-            return False
-        
-        user = {
-            user_data['id']: { 
-                'name': user_data['name'],
-                'contact': user_data['contact'],
-                'address': user_data['address'],
-            }
-        }
-        
-        data.update(user)
-        with open(path, 'w', encoding='utf-8') as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
-        return True
-
-    def all_info_user(self) -> dict | bool:
-        path = self.path
-        with open(path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-        
-        active_users = { }
-        for key, item in data.items():
-            if item['status']:
-                active_users[key] = item
-                
-        if not len(active_users):
-            return False
-                
-        return active_users
     
     def delete_user(self, id: str) -> bool:
         path = self.path
@@ -94,7 +58,35 @@ class Registration:
             json.dump(data, file, indent=4, ensure_ascii=False)
         return True
     
-    def show_user(self, id) -> dict | bool:
+    def update_user(self, **user_data: dict) -> bool:
+        path = self.path
+        with open(path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        if user_data['id'] not in data:
+            return False
+
+        if (not user_data['name']) or (not user_data['telephone']) or (not user_data['address']):
+            return False
+
+        if not data[user_data['id']]['status']:
+            return False
+        
+        user = {
+            user_data['id']: { 
+                'name': user_data['name'],
+                'telephone': user_data['telephone'],
+                'address': user_data['address'],
+                'status': user_data['status']
+            }
+        }
+        
+        data.update(user)
+        with open(path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+        return True
+
+    def info_user(self, id) -> dict | bool:
         path = self.path
         with open(path, 'r', encoding='utf-8') as file:
             data = json.load(file)
@@ -106,3 +98,18 @@ class Registration:
                 return False
                 
         return False
+    
+    def all_info_users(self) -> dict | bool:
+        path = self.path
+        with open(path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        
+        active_users = { }
+        for key, item in data.items():
+            if item['status']:
+                active_users[key] = item
+                
+        if not len(active_users):
+            return False
+                
+        return active_users
