70c6c556ed8e80730ce9de3f3b5a4d101f82b78e
MatBrands_B550_Arch <matheusmibrandao@gmail.com>
2023-01-27 10:38:33 -0300

    Update:

--- registration_system/model/Registration.py
+++ registration_system/model/Registration.py
@@ -86,7 +86,7 @@ class Registration:
             json.dump(data, file, indent=4, ensure_ascii=False)
         return True
 
-    def info_user(self, id) -> dict | bool:
+    def info_user(self, id: str) -> dict | bool:
         path = self.path
         with open(path, 'r', encoding='utf-8') as file:
             data = json.load(file)
